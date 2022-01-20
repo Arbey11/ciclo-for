941094f94b2aa3482677245a43d649aa86513d19
Didier <sasdier@hotmail.com>
2022-01-20 18:17:02 -0500

    Ciclo for imprimiendo vertical

---
+++ ciclo_for.py
@@ -0,0 +1,4 @@
+nume = int(input('Igrese numero: ')) 
+for i in range(nume, -1, -2):  # Este rango garantiza la cuenta hacia atras de 2 en 2
+    print(i, end=', ')  # end permite que en vez de imprimir vertical lo haga horizontal separado por comas
+    print()
